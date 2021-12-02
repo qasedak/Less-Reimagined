2fc8f07d9fa858e723922a7f8b73157387541b26
macse <m_anbarestany@hotmail.com>
2021-12-02 19:24:46 +0330

    bug fixes

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     if ($color_scheme === false) $color_scheme = 'light';  // fallback
 	if (isNight($_SERVER['REMOTE_ADDR'],$color_scheme)) {
 		echo '<link rel="stylesheet" type="text/css" href="' . get_template_directory_uri() . '/dark.css">';
+        echo '<meta name="theme-color" content="#0d0d0d">';
 	}
 	?>
 	<meta charset="<?php bloginfo('charset'); ?>" />
