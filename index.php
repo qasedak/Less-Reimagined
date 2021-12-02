265c4e9bed1d585f52c9d91b7658a08f89c7b252
macse <m_anbarestany@hotmail.com>
2021-12-02 20:30:53 +0330

    bug fixes

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width" />
 
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
 
 	<?php wp_head(); ?>
 </head>
@@ -264,7 +264,7 @@
     <script src="https://cdn.jsdelivr.net/npm/js-cookie/dist/js.cookie.min.js"></script>
     <script>
         // code to set the `color_scheme` cookie
-        var $color_scheme = Cookies.get("color_scheme");
+        const $color_scheme = Cookies.get("color_scheme");
         function get_color_scheme() {
             return (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) ? "dark" : "light";
         }
@@ -272,11 +272,11 @@
             Cookies.set("color_scheme", get_color_scheme());
         }
         // read & compare cookie `color-scheme`
-        if ((typeof $color_scheme === "undefined") || (get_color_scheme() != $color_scheme))
+        if ((typeof $color_scheme === "undefined") || (get_color_scheme() !== $color_scheme))
             update_color_scheme();
         // detect changes and change the cookie
         if (window.matchMedia)
-            window.matchMedia("(prefers-color-scheme: dark)").addListener( update_color_scheme );
+            window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", update_color_scheme );
     </script>
 </body>
 
